a15fafa8d2c33a2f68ebe86da09b94028f642fb2
Simon Walz <contact@simonwalz.de>
2021-04-03 17:33:03 +0200

    test: +Message

--- test/test.sh
+++ test/test.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+cd test/
+
 for i in *.json
 do
 	echo "$i"
 	mdfilename="$(basename "$i" ".json").md"
 	../cli.js --schema "$i" >"${mdfilename}"
 done
+
+if test "x$(git status -s *.md)" != "x"
+then
+	echo "Error: Generated Files have changed!" 2>&1
+	exit 1
+fi
+
+exit 0
